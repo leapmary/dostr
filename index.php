4aef371864dff480bdcbacfbcecb8545209ece30
leapmary <858956665@qq.com>
2016-08-06 15:11:31 +0800

    第一个包测试

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use Ford\Escape as Escape;
+use Ford\Fiesta as Fiesta;
+use Ford\Focus as Focus;
+use Ford\Fusion as Fusion;
+
+echo Fiesta\Fiesta2013::info();
+
+
+
+
+
+
+
+
+?>
